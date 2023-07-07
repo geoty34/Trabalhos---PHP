ba65a3a8659e07cea23c2cd8465f0f8cba5c8b40
Geovana Santos <101215198+geoty34@users.noreply.github.com>
2023-07-07 14:21:05 -0300

    Trabalho Album de desenhos

---
+++ Ativ_3Bim/desenho.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
+    <title>Album de desenhos animados</title>
+</head>
+<body>
+
+
+
+    <form action="" method="post">
+        <h1>Album de desenhos</h1>
+        <input type="text" name="nome" placeholder="Informe o nome"><br><br>
+
+        <input type="text" name="per_favorito" placeholder="Informe seu personagem favorito"><br><br>
+
+        <input type="number" name="ano" placeholder="Informe o Ano"><br><br>
+
+        <strong>Selecione a cor de fundo do album:</strong>
+        <!--<label for="cor" style="font-family: ;" >Selecione a cor do album:</label>-->
+        <input type="color"><br><br>
+
+
+        <input type="submit" value="Criar">
+
+
+
+
+
+
+    </form>
+</body>
+</html>
